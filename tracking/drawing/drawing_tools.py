0ffa076fc15a460c493972b59a1088d34bf7e571
Qu1nel <quinelleave@gmail.com>
2021-11-17 10:49:40 +0300

    implementation of funcs 1.draw_circle (drawing_tools.py) 2.draw_fill_circle (drawing_tools.py)

--- tracking/drawing/drawing_tools.py
+++ tracking/drawing/drawing_tools.py
@@ -1,10 +1,12 @@
 from typing import Union, List, Tuple
+
 import cv2
 from numpy import ndarray
 
 from .colors import *
 from ..bodyparts.base_solution import Landmark
 
+__doc__ = """Just redefined funcs)"""
 __all__ = (
     'draw_line',
     'draw_circle',
@@ -20,29 +22,54 @@ def draw_line(image: ndarray,
               point2: Union[Landmark, Union[List[int], Tuple[int]]],
               color: Union[Tuple[int], List[int]] = RED,
               thickness: Union[None, int] = None) -> None:
-    """ Just redefined funcs)
-
+    """
     The function line draws the line segment between pt1 and pt2 points in the image. The line is
     .   clipped by the image boundaries. For non-antialiased lines with integer coordinates, the 8-connected
     .   or 4-connected Bresenham algorithm is used. Thick lines are drawn with rounding endings. Antialiased
     .   lines are drawn using Gaussian filtering.
 
-    :param image: Image
-    :param point1: Landmark object or sequence with two coord statement
-    :param point2: Landmark object or sequence with two coord statement
-    :param color: The sequence contains a three values. RGB
-    :param thickness: Int. Line thickness value
+    :param image: Image.
+    :param point1: Landmark object or sequence with two coord statement.
+    :param point2: Landmark object or sequence with two coord statement.
+    :param color: The sequence contains a three values. RGB.
+    :param thickness: Int. Line thickness value.
     :return: None
     """
     cv2.line(image, point1, point2, color, thickness)
 
 
-def draw_circle():
-    pass
+def draw_circle(image: ndarray,
+                center: Union[Landmark, Union[List[int], Tuple[int]]],
+                radius: int,
+                color: Union[Tuple[int], List[int]] = RED,
+                thickness: Union[None, int] = None) -> None:
+    """
+    Draws a circle.
+
+    :param image: Image.
+    :param center: Center of the circle.
+    :param radius: Radius of the circle.
+    :param color: The sequence contains a three values. RGB.
+    :param thickness: Int. Circle thickness value.
+    :return: None
+    """
+    cv2.circle(image, center, radius, color, thickness)
 
 
-def draw_fill_circle():
-    pass
+def draw_fill_circle(image: ndarray,
+                     center: Union[Landmark, Union[List[int], Tuple[int]]],
+                     radius: int,
+                     color: Union[Tuple[int], List[int]] = RED) -> None:
+    """
+    Draws a circle.
+
+    :param image: Image.
+    :param center: Center of the circle.
+    :param radius: Radius of the circle.
+    :param color: The sequence contains a three values. RGB.
+    :return: None
+    """
+    cv2.circle(image, center, radius, color, -1)
 
 
 def draw_rect():
--- tracking/main.py
+++ tracking/main.py
@@ -7,6 +7,8 @@ from .bodyparts.base_solution import Landmark
 def find_distance(point_one: Union[Landmark, Union[List[int], Tuple[int]]],
                   point_two: Union[Landmark, Union[List[int], Tuple[int]]]) -> float:
     """
+    Returns distance between point_one and point_two
+
     :param point_one: Landmark object or sequence with two coord statement
     :param point_two: Landmark object or sequence with two coord statement
     :return: float of num, I.e. distance between point_one and point_two
@@ -14,3 +16,17 @@ def find_distance(point_one: Union[Landmark, Union[List[int], Tuple[int]]],
     if isinstance(point_one, Landmark) and isinstance(point_two, Landmark):
         return math.sqrt((point_one.x - point_two.x) ** 2 + (point_one.y - point_two.y) ** 2)
     return math.sqrt((point_one[0] - point_two[0]) ** 2 + (point_one[1] - point_two[1]) ** 2)
+
+
+def find_coord_center_point_distance(point_one: Union[Landmark, Union[List[int], Tuple[int]]],
+                         point_two: Union[Landmark, Union[List[int], Tuple[int]]]) -> Tuple[int, int]:
+    """
+    Returns the coords of the center point of the distance between point_one and point_two
+
+    :param point_one: Landmark object or sequence with two coord statement
+    :param point_two: Landmark object or sequence with two coord statement
+    :return: Int of num
+    """
+    if isinstance(point_one, Landmark) and isinstance(point_two, Landmark):
+        return int((point_one.x + point_two.x) / 2), int((point_one.y + point_two.y) / 2)
+    return int((point_one[0] + point_two[0]) / 2), int((point_one[1] + point_two[1]) / 2)
