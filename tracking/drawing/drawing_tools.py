049b45d31c795b578dc8b7a7bc5054642ea20efd
Qu1nel <quinelleave@gmail.com>
2021-11-17 08:54:19 +0300

    extra funcs module drawing_tools: 1.draw_line 2.draw_circle (fill) 3.draw_rect (fill) 4.draw_text module main: 1.find_distance

---
+++ tracking/drawing/drawing_tools.py
@@ -0,0 +1,22 @@
+def draw_line():
+    pass
+
+
+def draw_circle():
+    pass
+
+
+def draw_fill_circle():
+    pass
+
+
+def draw_rect():
+    pass
+
+
+def draw_fill_rect():
+    pass
+
+
+def draw_text():
+    pass
