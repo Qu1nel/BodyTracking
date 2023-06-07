c5c28cffd39addd9daf4b2f97e0c52970f7f0bff
Ivan Covach <eeiprex@gmail.com>
2023-06-07 18:18:22 +0300

    [chore] Update `__init__.py` in bodyparts pkg
    Files:
        [c] tracking/bodyparts/__init__.py

--- tracking/bodyparts/__init__.py
+++ tracking/bodyparts/__init__.py
@@ -1,3 +1,4 @@
 from tracking.bodyparts import FaceDetection
 from tracking.bodyparts import FaceMeshDetection
 from tracking.bodyparts import HandTracking
+from tracking.bodyparts import PoseDetection
