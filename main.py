f310587ae624d2eba2fd562e71abdf5d031bd1c0
Ivan Covach <eeiprex@gmail.com>
2023-06-07 18:18:49 +0300

    [chore] Total update `main.py`
    Files:
        [c] main.py
    
    Changes:
        Added support for exploitation pose detection.

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from tracking import DrawingUtils
 from tracking.bodyparts.HandTracking import HandsDetector, Hand
 from tracking.bodyparts.FaceMeshDetection import FacesMeshDetector, FaceMesh
 from tracking.bodyparts.FaceDetection import FacesDetector, Face
+from tracking.bodyparts.PoseDetection import PoseDetector, Pose
 from tracking.bodyparts.base_solution import LabelMixin
 
 TITLE_WINDOW = 'Tracking'
 DEFAULT_HAND_CONNECTIONS = connections.HandConnections.HAND_CONNECTIONS_V2
 DEFAULT_FACEMESH_TESSELATION = connections.FaceConnections.FACEMESH_TESSELATION
+DEFAULT_POSE_CONNECTIONS = connections.PoseConnections.POSE_CONNECTIONS_NO_PALM
 
 
 def pressed_exit() -> bool:
@@ -25,9 +27,9 @@ def status_good(status: Any) -> bool:
 
 def main(source) -> None:
     with HandsDetector(min_detection_confidence=0.7, min_tracking_confidence=0.2, max_num_hands=2) as HandObj, \
-            FacesMeshDetector() as FaceMeshObj, FacesDetector() as FaceObj:
+            FacesMeshDetector() as FaceMeshObj, FacesDetector() as FaceObj, PoseDetector() as PoseObj:
 
-        handler_objects = [HandObj, FaceObj, FaceMeshObj]
+        handler_objects = [HandObj, FaceObj, PoseObj, FaceMeshObj]
 
         while source.isOpened():
             status, image = camera.read()
@@ -36,23 +38,25 @@ def main(source) -> None:
                 print("Ignoring empty camera frame.")
                 break
 
-            result_processing: List[Union[Hand, Face, FaceMesh, LabelMixin]] = [
+            result_processing: List[Union[Pose, Hand, Face, FaceMesh, LabelMixin]] = [
                 entity for obj in handler_objects for entity in obj.process(image)
             ]
 
             for result in result_processing:
                 label = result.name
 
-                if label in ('hand', 'facemesh'):
+                if label in ('hand', 'facemesh', 'pose'):
                     if label == 'hand':
                         connection = DEFAULT_HAND_CONNECTIONS
                     elif label == 'facemesh':
                         connection = DEFAULT_FACEMESH_TESSELATION
+                    elif label == 'pose':
+                        connection = DEFAULT_POSE_CONNECTIONS
 
-                    DrawingUtils.draw_landmarks(image, result, connections=connection)
+                    DrawingUtils.draw_landmarks(image, result, connections=connection, smart_drawing=True)
 
                 elif label == 'face':
-                    DrawingUtils.draw_detection(image, result)
+                    DrawingUtils.draw_detection(image, result, draw_landmark=False)
                 else:
                     raise RuntimeError
 
